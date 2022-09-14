1b96ceda214dcd28990aebfc574963380cfeab0c
Olga Ngoni <ngoniolga@gmail.com>
2022-09-14 16:42:57 -0700

    Combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * main-Entry point
+ * Return:0 when success
+ */
+int main(void)
+{
+	int m, n;
+
+	for (m = 48; m <= 57; m++)
+	{
+		for (n = 48; n <= 57; n++)
+		{
+			if (m < n)
+			{
+				putchar(m);
+				putchar(n);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar(10);
+}
