19e8eaf8334db6cdd41ea928c73c088b6c18b7fc
Olga Ngoni <ngoniolga@gmail.com>
2022-09-15 04:07:56 -0700

    Is lower

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha-All alphabets
+ * c@:input
+ * Return:1 if alphabet else 0
+ */
+int _isalpha(int c)
+{
+	int result;
+
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		result = 1;
+	}
+	else
+		result = 0;
+	return (result);
+}
