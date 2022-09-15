63583579d05050593a34e6fbfb11868654bdc0f7
Olga Ngoni <ngoniolga@gmail.com>
2022-09-14 17:04:01 -0700

    Combinations

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+/**
+ * main-Entry
+ * Return:0 when success
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 48; a <= 57; a++)
+	{
+		for (b = 48; b <= 57; b++)
+		{
+			for (c = 48; c <= 57; c++)
+			{
+				if (a < b && a < c && b < c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a != 55, b != 56, c != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
