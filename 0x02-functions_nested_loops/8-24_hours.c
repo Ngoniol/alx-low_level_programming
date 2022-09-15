9e947e090331c195f114d242272a48d8f58a6612
Olga Ngoni <ngoniolga@gmail.com>
2022-09-15 05:43:12 -0700

    Time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer-print minutes in an hour
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 48; a <= 50; a++)
+	{
+		for (b = 48; b <= 57; b++)
+		{
+			for (c = 48; c <= 53; c++)
+			{
+				for (d = 48; d <= 57; d++)
+				{
+					_putchar(a);
+					_putchar(b);
+					_putchar(':');
+					_putchar(c);
+					_putchar(d);
+					_putchar(10);
+				}
+			}
+		}
+	}
+}
