8b25bfcbc48fba65d1fd8920d61d33988430f5be
Olga Ngoni <ngoniolga@gmail.com>
2022-09-15 12:34:48 -0700

    Times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * times_table-multiplication
+ */
+void times_table(void)
+{
+	int x, y, k;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <10; y++)
+		{
+			k = x * y;
+			if (y == 0)
+			{
+				_putchar(k + '0');
+			}
+			if (y != 0 && k < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
