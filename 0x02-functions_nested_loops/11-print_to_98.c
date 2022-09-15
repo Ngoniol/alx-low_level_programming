b4786b0680cee416247dab5caf3ae57675f48d1e
Olga Ngoni <ngoniolga@gmail.com>
2022-09-15 13:36:46 -0700

    To add

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_to_98-Print to 98
+ * @n:input
+ */
+void print_to_98(int n)
+{
+	int num;
+
+	for (num = n; num <= 98; num++)
+	{
+		printf("%d", n);
+		if (num != 98)
+		{
+			printf(", ");
+		}
+		if (num == 0)
+		{
+			printf("\n");
+		}
+	}
+}
