3727b455e81b592d22b71dc97299f01a7f70fba1
Olga Ngoni <ngoniolga@gmail.com>
2022-09-15 12:55:58 -0700

    Add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,12 +2,11 @@
 
 /**
  * add-adds two integers
+ * @a:input
+ * @b:input
  * Return:results
  */
 int add(int a, int b)
 {
-	int result;
-
-	result = a + b;
-	return (result);
+	return (a + b);
 }
