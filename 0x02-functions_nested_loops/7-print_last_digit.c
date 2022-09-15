ccd9090777962674e8be93ce4181ff8939f78f11
Olga Ngoni <ngoniolga@gmail.com>
2022-09-15 05:11:47 -0700

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit-last digit is printed
+ * @n:input
+ * Return:value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int result;
+	
+	result = result % 10;
+	return (result);
+}
