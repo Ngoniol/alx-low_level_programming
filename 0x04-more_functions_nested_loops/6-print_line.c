415088595544b269d6f532d60110b93e7a1970af
Olga Ngoni <ngoniolga@gmail.com>
2022-09-19 06:14:01 -0700

    straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * print_line-Draws a straight line
+ * @n:input
+ */
+void print_line(int n)
+{
+	int count;
+
+	if (n <= 0)
+		_putchar(10);
+	else
+	{
+		for (count = 0; count <= n; count++)
+		{
+			_putchar(95);
+		}
+		_putchar(10);
+	}
+}
