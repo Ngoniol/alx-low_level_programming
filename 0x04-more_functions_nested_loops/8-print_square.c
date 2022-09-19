55cb38a58484ac2d2d7d49cb118a3f20859cdff8
Olga Ngoni <ngoniolga@gmail.com>
2022-09-19 08:01:02 -0700

    Square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square-prints square
+ * @size:size of the same
+ */
+void print_square(int size)
+{
+	int count;
+
+	if (n <= 0)
+		_putchar(10);
+	else
+	{
+		for (count = 0; count <= size; count++)
+		{
+			for (count = 0; count <= size; count++)
+			{
+				_putchar(35);
+			}
+			_putchar(10);
+		}
+	}
+}
