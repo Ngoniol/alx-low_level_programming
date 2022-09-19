118aee6c9eae509320e697143f077dadf2664d4a
Olga Ngoni <ngoniolga@gmail.com>
2022-09-19 04:43:00 -0700

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_numbers--prints numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	int count;
+
+	for (count = 48; count <= 57; count++)
+		_putchar(count);
+	_putchar(10);
+}
