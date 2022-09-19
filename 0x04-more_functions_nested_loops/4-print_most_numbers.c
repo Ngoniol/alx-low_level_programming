3146232d4b1679d24404972a7770d21123abadf1
Olga Ngoni <ngoniolga@gmail.com>
2022-09-19 05:07:53 -0700

    Most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * print_most_numbers-prints all digits apart from 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int count;
+
+	for (count = 48; count <= 57; count++)
+	{
+		if ((count != 50) && (count != 52))
+			_putchar(count);
+	}
+	_putchar(10);
+}
