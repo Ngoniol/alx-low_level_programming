da93f5d940f6d4c264576065956d33c0ead4358f
Olga Ngoni <ngoniolga@gmail.com>
2022-09-18 22:53:14 -0700

    Positive negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main-tests positive negative function
+ * Return:0 when successful
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
