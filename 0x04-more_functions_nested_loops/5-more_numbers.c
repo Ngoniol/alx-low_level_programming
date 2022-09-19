1cc73642c031cd708b4028e37de8dd4909a763e7
Olga Ngoni <ngoniolga@gmail.com>
2022-09-19 05:41:30 -0700

    Most numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers-Prints ten times the numbers 0 to 14
+ */
+void more_numbers(void)
+{
+	int count, times, a, b;
+
+	for (times = 0; times < 10; times++)
+	{
+		for (count = 48; count <= 57; count ++)
+		{
+			_putchar(count);
+		}
+		a = 49;
+		for (b = 48; b < 53; b++)
+		{
+			_putchar(a);
+			_putchar(b);
+		}
+	}
+	_putchar(10);
+}
