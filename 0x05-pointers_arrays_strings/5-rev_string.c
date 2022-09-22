1509f340f9f8605e42303e2d78325a7d6757b4da
Olga Ngoni <ngoniolga@gmail.com>
2022-09-22 04:50:54 -0700

    Reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * rev_string-Reverses string
+ * @s:input string
+ */
+void rev_string(char *s)
+{
+	int count = 0, num = 0;
+
+	while (s[count++])
+	{
+		num++;
+	}
+	for (count = num -1; count >= 0; count--)
+	{
+		_putchar(s[count]);
+	}
+	_putchar(10);
+}
