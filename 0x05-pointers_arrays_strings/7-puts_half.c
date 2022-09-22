c3dc60cc56a9907ad7e330615eece7fd8689e89f
Olga Ngoni <ngoniolga@gmail.com>
2022-09-22 05:38:54 -0700

    Puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts_half-prints half of a string
+ * @str:Input
+ */
+void puts_half(char *str)
+{
+	int count = 0, num;
+
+	while (str[count++])
+		count++;
+	count = count / 2;
+	for (num = 0; num <= count; num++)
+	{
+		_putchar(s[num]);
+	}
+	_putchar(10);
+}
